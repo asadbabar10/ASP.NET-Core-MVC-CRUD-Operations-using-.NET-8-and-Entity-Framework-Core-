7615c275faa47a739bba60e94cf1999e716269ef
eswstation1 <eswstation1@gmail.com>
2025-07-02 19:01:04 +0500

    in it

---
+++ StudentPortal.Web/Controllers/ApplicationDbConstext.cs
@@ -0,0 +1,12 @@
+﻿namespace StudentPortal.Web.Controllers
+{
+    public class ApplicationDbConstext
+    {
+        public object Students { get; internal set; }
+
+        internal Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
